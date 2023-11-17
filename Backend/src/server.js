825dadda564952aeb07d7d5bd2174df65bf77b22
ankit kumar <ankitduvedi.03@gmail.com>
2023-11-17 20:19:19 +0530

    first commit

---
+++ Backend/src/server.js
@@ -0,0 +1,27 @@
+
+const express = require('express');
+const app = express();
+
+const mongoose = require('mongoose');
+const Note = require('./models/note');
+
+const bodyParser = require('body-parser');
+const note = require('./models/note');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+mongoose.connect("mongodb+srv://ankitduvedi03:ankitduvedi03@cluster0.y58dpwq.mongodb.net/notesdb").then(function(){
+    app.get("/",function(req,res){
+        const response = {message: "API works this is a APP from WHILE"}
+        res.json(response); 
+    });
+    // notes route
+    const noteRouter= require('./routes/Note');
+    app.use("/notes", noteRouter);
+});
+
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, function(){
+    console.log("Server started at port 3000");
+});
